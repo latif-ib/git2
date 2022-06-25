13735aafc73a6ee643b5b4e83d122b21fba51359
latif-ib <iblatus@gmail.com>
2022-06-26 07:40:46 +0900

    scatter plot implemented

--- testfile1.R
+++ testfile1.R
@@ -1,9 +1,15 @@
 library(tidyverse)
 library(palmerpenguins)
+summary(penguins)
+
 penguins %>% 
   ggplot(aes(x=bill_depth_mm))+
-  geom_histogram()
+geom_histogram()
+
 # hi there
 penguins %>% 
-  ggplot(aes(x=bill_depth_mm))+
-geom_points()
+  drop_na() %>% 
+  ggplot(aes(bill_length_mm, bill_depth_mm,color=species))+
+  geom_point()
+
+
